3aaadb767b31d6f50855b42c8669d454c65a6859
Kyoo130 <kml9003@kakao.com>
2021-12-24 11:11:48 +0900

    🚚 useCharactersData > useFuturamaData

--- src/hooks/useCharactersData.ts
+++
@@ -1,7 +0,0 @@
-import useSWR from 'swr';
-import { fetcher } from '../utils/fetcher';
-import { CHARACTERS_API_ENDPOINT } from '../constants';
-
-export const useCharactersSData = (path: string) => {
-  return useSWR(`${CHARACTERS_API_ENDPOINT}${path}`, fetcher);
-}
