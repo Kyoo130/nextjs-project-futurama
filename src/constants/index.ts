c143c047959b7f66f0eda460760a458476d77641
Kyoo130 <kml9003@kakao.com>
2021-12-26 00:28:17 +0900

    ✨ Layout 구현

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,47 @@
 export const FUTURAMA_API_ENDPOINT = 'https://api.sampleapis.com/futurama/';
+
+export const ROUTES = [
+    {
+        ID: 0,
+        PATH: '/futurama',
+        LABEL: 'Futurama',
+        SUBS: [
+            {
+                ID: 0,
+                PATH: '/info',
+                LABEL: 'Info',
+                ORDER: 0,                   
+            },
+            {
+                ID: 1,
+                PATH: '/characters',
+                LABEL: 'Characters',
+                ORDER: 1,                   
+            },
+            {
+                ID: 2,
+                PATH: '/cast',
+                LABEL: 'Cast',
+                ORDER: 2,                   
+            },
+            {
+                ID: 3,
+                PATH: '/episodes',
+                LABEL: 'Episodes',
+                ORDER: 3,                   
+            },
+            {
+                ID: 4,
+                PATH: '/questions',
+                LABEL: 'Questions',
+                ORDER: 4,                   
+            },
+            {
+                ID: 5,
+                PATH: '/inventory',
+                LABEL: 'Inventory',
+                ORDER: 5,                   
+            },
+        ]
+    }
+]
