60527884bd9f35e48471bb03cba4b326bb86a480
Kyoo130 <kml9003@kakao.com>
2021-12-27 01:34:46 +0900

    💄 main 페이지 스타일 적용

--- src/pages/futurama/index.tsx
+++ src/pages/futurama/index.tsx
@@ -1,11 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import styled from "@emotion/styled";
 import type { NextPage } from "next";
 
 const FuturamaPage: NextPage = () => {
   return (
-    <div>
-      <h1>FuturamaPage</h1>
-    </div>
+    <FuturamaCont>
+      <FuturamaH2>FUTURAMA</FuturamaH2>
+      <FuturamaTit src="https://www.denofgeek.com/wp-content/uploads/2018/06/futurama_4.jpg?resize=768%2C432" alt="logo" />
+      <FuturamaFooter>
+        <p>Copyright &copy; </p>
+      </FuturamaFooter>
+    </FuturamaCont>
   )
 }
 
 export default FuturamaPage;
+
+const FuturamaCont = styled.div`
+  text-align: center;
+`
+
+const FuturamaH2 = styled.h2 `
+  font-size: 2.5rem;
+`
+
+const FuturamaTit = styled.img`
+  width: 50%;
+  height: 50%;
+`
+
+const FuturamaFooter = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 3rem;
+  background-color: lightgray;
+`
