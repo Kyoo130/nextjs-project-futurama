76f77a5486787eeea5229609771f2043359b0c19
Kyoo130 <kml9003@kakao.com>
2021-12-28 00:34:09 +0900

    💄 Info Page CSS 적용

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Info } from "../types/info";
 
 interface InfoProps {
@@ -9,24 +10,11 @@ export const InfoCard = ({ infoData }: InfoProps) => {
 
   return (
     <div>
-      <dl>
-        <dt>synopsis</dt>
-        <dd>{synopsis}</dd>
-      </dl>
-      <dl>
-          <dt>yearsAired</dt>
-          <dd>{yearsAired}</dd>
-      </dl>
-      <dl>
-          <dt>creators - 1</dt>
-          <dd>{creators[0].name}</dd>
-          <dd>{creators[0].url}</dd>
-      </dl>
-      <dl>
-          <dt>creators - 2</dt>
-          <dd>{creators[1].name}</dd>
-          <dd>{creators[1].url}</dd>
-      </dl>
+      <h2>Synopsis</h2>
+      <small>{yearsAired}</small>
+      <p>{synopsis}</p>
     </div>
   );
 };
+
+
--- src/pages/futurama/info.tsx
+++ src/pages/futurama/info.tsx
@@ -1,7 +1,9 @@
+import styled from "@emotion/styled";
 import type { NextPage } from "next";
 import { InfoCard, Error, Loading } from "../../components";
 import { useFuturamaData } from "../../hooks/useFuturamaData";
-import { Info } from "../../types/info";
+import { Info } from "../../types/Info";
+
 
 const InfoPage: NextPage = () => {
   const endpoint = "info";
@@ -12,8 +14,7 @@ const InfoPage: NextPage = () => {
 
   return (
     <div>
-      <h1>InfoPage</h1>
-      <main>
+      <InfoCont>
         {data.map((infoData: Info) => {
           return (
             <InfoCard
@@ -22,9 +23,18 @@ const InfoPage: NextPage = () => {
             />
           );
         })}
-      </main>
+      </InfoCont>
     </div>
   )
 }
 
 export default InfoPage;
+
+const InfoCont = styled.main`
+  width: 90%;
+  margin: 30px auto;
+  padding: 10px 20px;
+  border: 1px solid lightgray;
+  border-radius: 4px;
+  box-shadow: 3px 3px 5px 0px rgba(191, 191, 191, 0.53);
+`
